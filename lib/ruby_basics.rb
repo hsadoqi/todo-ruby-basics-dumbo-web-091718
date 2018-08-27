d468562ebe0991848294d3383d8a1af80b98f67c
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-27 01:50:53 +0000

    automatically backed up by learn

---
+++ lib/ruby_basics.rb
@@ -0,0 +1,23 @@
+def division (num1, num2)
+  return num1/num2
+end
+
+def assign_variable(name)
+  return name
+end
+
+def argue
+  
+end
+
+def greeting
+end
+
+def return_a_value
+end
+
+def last_evaluated_value
+end
+
+def pizza_party
+end
